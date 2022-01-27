0a9ebbcd8b1cce91f17effb65993f906748c99e6
Tangqy <tangqianyu123456@gmail.com>
2022-01-27 10:55:10 +0800

    no message

--- view/src/app/pages/auth/auth.module.ts
+++ view/src/app/pages/auth/auth.module.ts
@@ -1,31 +1,20 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { SharedModule } from "@shared";
-import { LoginComponent } from "./login/login.component";
-import {AdminComponent} from "../admin/admin.component";
-import {PostListComponent} from "../admin/post/post-list/post-list.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SharedModule } from '@shared';
+import { LoginComponent } from './login/login.component';
 import { CallbackComponent } from './callback/callback.component';
-
-
 
 const routes: Routes = [
   {
     path: '',
-    component: LoginComponent
-  }
-]
+    component: LoginComponent,
+  },
+];
 
 @NgModule({
-  declarations: [
-    LoginComponent,
-    CallbackComponent
-
-  ],
-  imports: [
-    SharedModule,
-    RouterModule.forChild(routes),
-  ],
+  declarations: [LoginComponent, CallbackComponent],
+  imports: [SharedModule, RouterModule.forChild(routes)],
   exports: [],
   providers: [],
 })
-export class AuthModule { }
+export class AuthModule {}
